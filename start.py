0788774953bdecacd277c1d71cc3670e3257aa84
Ilya Kotsar <ilyakotsar@yahoo.com>
2025-01-10 20:55:39 +0700

    Update

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+import os
 import random
 from cryptography.hazmat.primitives.asymmetric import dh
 from datetime import datetime
@@ -12,14 +13,19 @@ def generate() -> str:
 
 
 def main():
-    print('Generating...\n')
-    start = datetime.now()
-    parameters = generate()
-    print(f'Parameters: {parameters}')
-    time_spent = datetime.now() - start
-    seconds = round(time_spent.total_seconds())
-    print(f'\nTime spent: {seconds} sec')
-    input()
+    while True:
+        print('Generating...\n')
+        start = datetime.now()
+        parameters = generate()
+        print(f'Parameters: {parameters}')
+        time_spent = datetime.now() - start
+        seconds = round(time_spent.total_seconds())
+        print(f'\nTime spent: {seconds} sec')
+        input('\nPress Enter to repeat')
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
 
 
 if __name__ == '__main__':
